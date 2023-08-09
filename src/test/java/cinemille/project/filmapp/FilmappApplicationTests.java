fbe3888ef98cc2fc295cd4974b4077e3ffa83be7
Nourelhoda Essayeh <noure.essayeh@gmail.com>
2023-08-09 11:26:26 +0200

    created new project to manage and show films shown at cinema

---
+++ src/test/java/cinemille/project/filmapp/FilmappApplicationTests.java
@@ -0,0 +1,13 @@
+package cinemille.project.filmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
