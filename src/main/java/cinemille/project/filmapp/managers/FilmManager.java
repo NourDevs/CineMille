034f478dabb22a1ace34e9404bac6ac629bf9761
Nourelhoda Essayeh <noure.essayeh@gmail.com>
2023-08-09 23:14:16 +0200

    added managers

--- src/main/java/cinemille/project/filmapp/managers/FilmManager.java
+++ src/main/java/cinemille/project/filmapp/managers/FilmManager.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/film")
+@RequestMapping("/api/v1/film")
 public class FilmManager {
 
     private final FilmService filmService;
